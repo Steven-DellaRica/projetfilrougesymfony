c06aecc621197796fdd6ea5a30d1cf80260c3da1
StevenD <dellarica.steven@gmail.com>
2023-10-24 22:54:07 +0200

    form css

--- src/Form/VideoIDType.php
+++ src/Form/VideoIDType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Videos;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,10 @@ class VideoIDType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('video_id')
+            ->add('video_id', TextType::class, [
+                'label' => 'Lien de la vidéo :',
+                'attr' => ['class' => 'videoIdclass'],
+            ])
         ;
     }
 
